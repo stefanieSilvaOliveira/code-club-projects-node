98961c0b939cabb7f80a0a3fff6c261bb56336be
bonecahStefanie <stefaniesilvadeoliveira@yahoo.com.br>
2023-11-27 21:31:14 -0300

    meu primeiro projeto node.js

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const uuid = require('uuid')
+
+const port = 3000
+const app = express()
+app.use(express.json())
+
+
+const users = []
+
+const checkUserId = (request, response, next) => {
+     const { id } = request.params
+
+     const index = users.findIndex(user => user.id === id)
+
+     if (index < 0) {
+          return response.status(404).json({ error: "User not Found" })
+     }
+
+     request.userIndex = index
+     request.userId = id
+
+     next()
+}
+
+app.get('/users', (request, response) => {
+
+     return response.json(users)
+
+})
+
+app.post('/users', (request, response) => {
+
+     const { name, age } = request.body
+
+     const user = { id: uuid.v4(), name, age }
+
+     users.push(user)
+
+     return response.status(201).json(users)
+
+})
+
+app.put('/users/:id',checkUserId,(request, response) => {
+     const { name, age } = request.body
+     const index = request.userIndex
+     const id = request.userId
+     
+     const updatedUser = { id, name, age }
+
+     users[index] = updatedUser
+
+     return response.json(updatedUser)
+
+})
+
+app.delete('/users/:id',checkUserId, (request, response) => {
+     const index = request.userIndex
+
+     users.splice(index, 1)
+
+     return response.status(204).json()
+})
+
+app.listen(3000, () => {
+     console.log('😜 Server started on port 3000!')
+})
